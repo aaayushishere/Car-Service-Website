5b31da635c157a3a280542ba5330e4f9ab7c8ae7
Ayush Saxena <aayushsaxenaaayush@gmail.com>
2024-07-29 06:55:53 +0530

    4

--- src/component/homepage/Navbottom.jsx
+++ src/component/homepage/Navbottom.jsx
@@ -4,7 +4,7 @@ const Navbottom = () => {
   return (
     <div>
         <div className=' w-full h-[90vh] flex'>
-<img src="https://thumbs.dreamstime.com/b/auto-mechanic-car-suspension-repair-work-28685626.jpg" className="object-cover w-full h-full " alt="" />
+<img src="https://www.seat.com/content/dam/public/seat-website/seat-cars/car-maintenance/article-single-image-maintenance/seat-services-and-repair-maintenance.jpg" className="object-cover w-full h-full " alt="" />
   
 </div>
     </div>
